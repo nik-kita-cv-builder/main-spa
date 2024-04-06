900100831a49e8ae4b33e35ba57f76b78acf2b79
nik-kita <nik-kita.deno.dev>
2024-04-07 01:32:59 +0200

    env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,18 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+/// <reference types="npm:vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
